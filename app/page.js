96081266db24b5a477d2c4129872d8e274e85e50
MemoSy <ahmeddalhalabi1@gmail.com>
2023-09-03 14:25:33 +0300

    video update

--- app/page.js
+++ app/page.js
@@ -1,9 +1,16 @@
+import Posts from '@/Components/Posts'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
-      Home
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <Posts />
+          </div>
+        </div>
+      </section>
     </>
   )
 }
